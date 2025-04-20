c7c2fbff8eb23b505d4ec0199e69f033c6326334
Lostris201 <114565886+Lostris201@users.noreply.github.com>
2025-04-21 01:11:37 +0300

    Add Firebase web configuration

---
+++ probody_ems/web/firebase-config.js
@@ -0,0 +1,14 @@
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyA-BcDeFgHiJkLmNoPqRsTuVwXyZ123456",
+  authDomain: "probody-ems.firebaseapp.com",
+  projectId: "probody-ems",
+  storageBucket: "probody-ems.appspot.com",
+  messagingSenderId: "123456789012",
+  appId: "1:123456789012:web:uvwxyz1234567890abcde",
+  measurementId: "G-ABCDEFGHIJ"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
